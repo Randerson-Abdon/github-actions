c4c2c82e609ac1c5eb2070bbcd98ad0ad45996c6
Randerson Abdon <abdon@Randersons-MacBook-Pro.local>
2024-04-03 07:08:31 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>Pagina Inicial</h1>
+      <a href="/sobre">Sobre</a>
+    </div>
+  )
+}
---
+++ pages/sobre.js
@@ -0,0 +1,8 @@
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>Pagina Sobre</h1>
+      <a href="/">Home</a>
+    </div>
+  )
+}
